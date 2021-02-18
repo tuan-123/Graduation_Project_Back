dbd35879d8dad23145df6ff7f03e68d4d0b22338
tuan <1278991950@qq.com>
2021-02-18 17:03:02 +0800

    创建工程

---
+++ src/main/java/com/zsc/springboot/CampusHelperGraduationApplication.java
@@ -0,0 +1,13 @@
+package com.zsc.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusHelperGraduationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusHelperGraduationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zsc/springboot/CampusHelperGraduationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsc.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusHelperGraduationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
