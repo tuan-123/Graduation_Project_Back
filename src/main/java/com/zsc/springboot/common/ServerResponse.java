3f18c09544273df6e0f77342dab2072ad9cee59c
tuan <1278991950@qq.com>
2021-02-24 11:24:13 +0800

    统一响应

---
+++ src/main/java/com/zsc/springboot/common/ServerResponse.java
@@ -0,0 +1,54 @@
+package com.zsc.springboot.common;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+@Data
+public class ServerResponse {
+
+    private int code;
+    private String msg;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String token;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Object data;
+
+    private ServerResponse(int code,String msg){
+        this.code = code;
+        this.msg = msg;
+    }
+
+    private ServerResponse(int code, String msg, Object data){
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    private ServerResponse(int code, String msg, Object data, String token){
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+        this.token = token;
+    }
+
+    // 操作成功
+    public static ServerResponse success(Object data){
+        return new ServerResponse(ResponseCode.SUCCESS.getCode(), ResponseCode.SUCCESS.getMsg(),data);
+    }
+    public static ServerResponse addSuccess(){
+        return new ServerResponse(ResponseCode.SUCCESS.getCode(), ResponseCode.SUCCESS.getMsg(),"添加成功");
+    }
+    public static ServerResponse loginSuccess(Object data, String token){
+        return new ServerResponse(ResponseCode.SUCCESS.getCode(), ResponseCode.SUCCESS.getMsg(),data,token);
+    }
+
+    //操作失败
+    public static ServerResponse fail(String msg){
+        return new ServerResponse(ResponseCode.FAIL.getCode(),msg);
+    }
+    public static ServerResponse nullData(){
+        return new ServerResponse(ResponseCode.FAIL.getCode(),"查无数据");
+    }
+
+
+}
