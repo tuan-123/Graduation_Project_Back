eebc2d4c4a3b604bddf04aaecf51247964dbfd0e
tuan <1278991950@qq.com>
2021-04-01 23:22:30 +0800

    rabbitMq

--- src/main/java/com/zsc/springboot/rabbitMq/RabbitMQService.java
+++ src/main/java/com/zsc/springboot/rabbitMq/RabbitMQService.java
@@ -1,6 +1,9 @@
 package com.zsc.springboot.rabbitMq;
 
+import com.rabbitmq.client.Channel;
 import com.zsc.springboot.common.SendEmailCode;
+import com.zsc.springboot.webSocket.WebSocketEndpoint;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.QueueBinding;
@@ -11,6 +14,8 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+
 /**
  *
  *
@@ -20,28 +25,73 @@ import org.springframework.stereotype.Service;
 @Service
 public class RabbitMQService {
 
-    private static String subject = "团哥测试验证码";
+    private static String subject = "毕设";
     @Autowired
     private JavaMailSenderImpl javaMailSender;
     private static String fromUser;
     @Value("${spring.mail.username}")
     public void setFromUser(String from){
-        fromUser = from;
+        this.fromUser = from;
     }
 
 
-    @RabbitListener(bindings = @QueueBinding(value =
+    /*@RabbitListener(bindings = @QueueBinding(value =
                                 @Queue("fanout_queue_email"),exchange =
-                                @Exchange(value = "fanout_exchange",type = "fanout")))
-    public void psubConsumerEmailAno(SendEmailCode sendEmailCode) throws InterruptedException {
+                                @Exchange(value = "fanout_exchange",type = "fanout")))*/
+    @RabbitListener(queues = "fanout_queue_email")
+    public void psubConsumerEmailAno(SendEmailCode sendEmailCode, Message message, Channel channel) throws IOException {
+       try {
+            //String text = "您的本次注册验证码为：" + sendEmailCode.getCode() + ",有效时间为5分钟。\n祝您生活愉快！";
+            String text = sendEmailCode.getCode();
+            SimpleMailMessage msg = new SimpleMailMessage();
+            msg.setFrom(fromUser);
+            msg.setTo(sendEmailCode.getEmail());
+            msg.setSubject(subject);
+            msg.setText(text);
+            javaMailSender.send(msg);
+            channel.basicAck(message.getMessageProperties().getDeliveryTag(),false);
+        } catch (Exception e) {
+            e.printStackTrace();
+            channel.basicNack(message.getMessageProperties().getDeliveryTag(),false,true);
+        }
+        /*System.out.println(fromUser);
+        channel.basicAck(message.getMessageProperties().getDeliveryTag(),false);*/
+        /*System.out.println(sendEmailCode.getEmail());
+        System.out.println(sendEmailCode.getCode());
+        channel.basicAck(message.getMessageProperties().getDeliveryTag(),false);*/
 
-        String text = "您的本次注册验证码为：" + sendEmailCode.getCode() + ",有效时间为5分钟。\n祝您生活愉快！";
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(fromUser);
-        message.setTo(sendEmailCode.getEmail());
-        message.setSubject(subject);
-        message.setText(text);
-        javaMailSender.send(message);
+    }
 
+    @RabbitListener(queues = "direct_queue_orders_ask")
+    public void psubConsumerAskCountAno(Message message, Channel channel) throws IOException {
+        try {
+            WebSocketEndpoint.addAskCount();
+            channel.basicAck(message.getMessageProperties().getDeliveryTag(),false);
+        } catch (Exception e) {
+            e.printStackTrace();
+            channel.basicNack(message.getMessageProperties().getDeliveryTag(),false,true);
+        }
+    }
+
+    @RabbitListener(queues = "direct_queue_orders_help")
+    public void psubConsumerHelpCountAno(Message message, Channel channel) throws IOException {
+        try {
+            WebSocketEndpoint.addHelpCount();
+            channel.basicAck(message.getMessageProperties().getDeliveryTag(),false);
+        } catch (Exception e) {
+            e.printStackTrace();
+            channel.basicNack(message.getMessageProperties().getDeliveryTag(),false,true);
+        }
+    }
+
+    @RabbitListener(queues = "direct_queue_orders_idle")
+    public void psubConsumerIdleCountAno(Message message, Channel channel) throws IOException {
+        try {
+            WebSocketEndpoint.addIdleCount();
+            channel.basicAck(message.getMessageProperties().getDeliveryTag(),false);
+        } catch (Exception e) {
+            e.printStackTrace();
+            channel.basicNack(message.getMessageProperties().getDeliveryTag(),false,true);
+        }
     }
 }
